25c4d6af572020cd0cb71039ee91e62cf8b81a5a
Tiago Leite <Tiago.Leite@ctw.bmwgroup.com>
2022-07-11 13:03:32 +0100

    changes text in the home page

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>Yo Tiago!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sup Tiago
         </a>
       </header>
     </div>
